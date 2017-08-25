fa8d65db57f1178003f5ea9ad2a07e0d2741ce53
xiao lu <849837578@qq.com>
2017-08-25 13:13:08 +0800

    构建架子

---
+++ Ordering/api/modules/AccountSql.js
@@ -0,0 +1,32 @@
+var userSql = require('mysql');
+
+var database = '';
+
+module.exports = {
+	user: function(tableName,request,callback){
+
+		var connection = userSql.createConnection({
+		  host     : 'localhost',
+		  user     : 'root',
+		  password : '',
+		  database : database
+		});
+		connection.connect();
+		
+		var sql = 'SELECT * FROM user WHERE username = '+"'" +request.username+ "'"+' AND password =' + "'"+request.password +"'";
+		console.log('login',sql)
+
+		connection.query(sql,function (err, result) {
+			
+		   if(!err){
+			    if(callback && typeof callback == 'function'){
+			    	callback(result);
+			    }
+		    }else {
+		    	console.log('err', err);
+		    }
+		         
+		});
+		connection.end(); 
+	}
+};
