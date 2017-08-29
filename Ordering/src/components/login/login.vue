9f0a33f3a4765cc743920d284b5e9d5b5672ab68
RaoRong <171227471@qq.com>
2017-08-29 14:46:32 +0800

    login

--- Ordering/src/components/login/login.vue
+++ Ordering/src/components/login/login.vue
@@ -3,10 +3,10 @@
 		<h2>后  台  登   录</h2>
 		<el-form ref="ruleForm2" label-width="100px" class="demo-ruleForm">
 			<el-form-item label="用户名">
-				<el-input type="text" v-model="name"></el-input>
+				<el-input type="text" v-model="name" placeholder="请输入用户名"></el-input>
 			</el-form-item>
 			<el-form-item label="确认密码">
-				<el-input type="password" v-model="psw"></el-input>
+				<el-input type="password" v-model="psw" placeholder="请输入密码"></el-input>
 			</el-form-item>
 			<el-form-item class="center">
 				<el-button type="primary" @click="submitForm('ruleForm2')">登录</el-button>
@@ -29,18 +29,10 @@
 		},
 		methods: {
 			submitForm(formName) {
-				if(this.name!= '' && this.psw != ''){
+				if(this.name.trim() != '' && this.psw.trim() != ''){
 					console.log(this.name, this.psw)
 				}
 				
-				// this.$refs[formName].validate((valid) => {
-				// 	if (valid) {
-				// 		alert('submit!');
-				// 	} else {
-				// 		console.log('error submit!!');
-				// 		return false;
-				// 	}
-				// });
 			},
 			resetForm(formName) {
 				this.name = '';
