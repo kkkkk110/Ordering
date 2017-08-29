1dd487ccb2c2a468e7fa07a588665acd823f7f23
xiao lu <849837578@qq.com>
2017-08-29 14:14:31 +0800

    答辩前更新

--- Ordering/src/components/mainpage/mainpage.vue
+++ Ordering/src/components/mainpage/mainpage.vue
@@ -1,17 +1,18 @@
 <template>
 	<div class="mainpage">
 		<div class="mainpage1">
-			<router-link to="">前端点餐</router-link>
+			<router-link to="">.前端点餐</router-link>
 				
 		
 		</div>
 		<div class="mainpage2">
-			<router-link to="/cook">厨师实时操作</router-link>
+			<router-link to="/cook">.厨师实时操作</router-link>
 		</div>
 		<div class="mainpage3">
-			<router-link to="/serve">服务员诚意服务</router-link>
-
-			
+			<router-link to="/serve">.服务员诚意服务</router-link>
+		</div>
+		<div class="mainpage4">
+			<router-link to="/backstage">.后台管理</router-link>
 		</div>
 	
 	</div>
